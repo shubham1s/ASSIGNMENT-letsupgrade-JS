e59978e45e476b091a2efd3e185f3c375c38dc4c
shubham1s <48383087+shubham1s@users.noreply.github.com>
2020-07-20 15:51:55 +0530

    Add files via upload

---
+++ day 4/check.js
@@ -0,0 +1,12 @@
+var i;
+    for (i=0;i<100;i++)
+    
+   {
+        if (i%3==0 && i%15!=0)
+    console.log("fizz",i)
+   if(i%5==0 && i%15!=0)
+   console.log("buzz",i)
+   if(i%15==0)
+    console.log("fizzbuzz",i)
+}
+    
