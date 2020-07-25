53ad140fd3532ba50b3c69b790b24f83a32ae9cd
shubham1s <48383087+shubham1s@users.noreply.github.com>
2020-07-25 16:18:00 +0530

    Add files via upload

---
+++ day 4/shopping/spread.js
@@ -0,0 +1,22 @@
+// Spread Operator in Array
+
+let shopping_list = ['eggs ', 'pumpkin juice','real fruits -lichi', 'cold drinks '];
+
+let shopping_basket = ["bourbon ",...shopping_list,"mangoes"];
+console.log("current list:-",shopping_list);
+console.log("new added items:-",shopping_basket);
+// let person = {
+//     name:'Raj',
+//     age:25,
+//     city:'Raipur',
+// }
+
+
+// let employee = {
+//     ...person,
+//     salary:40000,
+//     designation:"Accountant"
+// }
+
+// console.log(person)
+// console.log(employee)
